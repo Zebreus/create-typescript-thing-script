6b9c5ead8c6e669203da4388e19dced0db35ef33
Zebreus <lennarteichhorn@googlemail.com>
2022-07-22 13:14:21 +0200

    Add js interface to create script

---
+++ index.ts
@@ -0,0 +1,44 @@
+import { exec } from "child_process"
+import path from "path";
+
+const sh = async (cmd: string) => {
+    return new Promise<{ stdout: string; stderr: string }>(function (resolve, reject) {
+      exec(cmd, (err, stdout, stderr) => {
+        if (err) {
+          reject(err)
+        } else {
+          resolve({ stdout, stderr })
+        }
+      })
+    })
+  }
+
+export type PackageSettings = {
+    path?: string
+    name?: string
+    description?: string
+    type?: "library" | "application"
+    monorepo?: boolean
+    repo?: string
+    branch?: string
+    authorName?: string
+    authorEmail?: string
+}
+
+export const createTypescriptThingScript = async (settings: PackageSettings) => {
+    const script = `/usr/bin/env bash ${path.resolve(__dirname, "create.sh")}`
+    const command = script + 
+    settings.path ? " --path " + settings.path : ""
+    + settings.name ? " --name " + settings.name : ""
+    + settings.description ? " --description " + settings.description : ""
+    + settings.type ? " --type " + settings.type : ""
+    + settings.monorepo ? " --in-monorepo" : ""
+    + settings.repo ? " --git-origin " + settings.repo : ""
+    + settings.branch ? " --git-branch " + settings.branch : ""
+    + settings.authorName ? " --author-name " + settings.authorName : ""
+    + settings.authorEmail ? " --author-email " + settings.authorEmail : ""
+
+    await sh(command)
+}
+
+export default createTypescriptThingScript
