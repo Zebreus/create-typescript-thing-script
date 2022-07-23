76ae0df733d1f9f334069e93d3e3903cda56aa71
Zebreus <lennarteichhorn@googlemail.com>
2022-07-23 23:31:09 +0200

    Add quotes around arguments

--- index.ts
+++ index.ts
@@ -35,15 +35,15 @@ export const createTypescriptThingScript = async (settings: PackageSettings) => 
   const script = `/usr/bin/env bash ${path.resolve(packagePath, "create.sh")}`
   const command =
     script +
-    (settings.path ? " --path " + settings.path : "") +
-    (settings.name ? " --name " + settings.name : "") +
-    (settings.description ? " --description " + settings.description : "") +
-    (settings.type ? " --type " + settings.type : "") +
+    (settings.path ? " --path '" + settings.path + "'": "") +
+    (settings.name ? " --name '" + settings.name + "'" : "") +
+    (settings.description ? " --description '" + settings.description + "'" : "") +
+    (settings.type ? " --type '" + settings.type + "'" : "") +
     (settings.monorepo ? " --in-monorepo" : "") +
-    (settings.repo ? " --git-origin " + settings.repo : "") +
-    (settings.branch ? " --git-branch " + settings.branch : "") +
-    (settings.authorName ? " --author-name " + settings.authorName : "") +
-    (settings.authorEmail ? " --author-email " + settings.authorEmail : "")
+    (settings.repo ? " --git-origin '" + settings.repo + "'" : "") +
+    (settings.branch ? " --git-branch '" + settings.branch + "'" : "") +
+    (settings.authorName ? " --author-name '" + settings.authorName + "'" : "") +
+    (settings.authorEmail ? " --author-email '" + settings.authorEmail + "'" : "")
 
   await sh(command)
 }
